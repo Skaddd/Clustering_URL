2a9a09005626b3fbf35e5ad177d813f5f7c3ea85
Timon <timon.fugier@orange.fr>
2021-02-03 12:01:55 +0100

    normalisation problem

--- main.py
+++ main.py
@@ -17,37 +17,96 @@ a.drop(a.columns[cols], axis=1,inplace=True)
 print(a.iloc[2])
 a.head(25)
 '''
+
+
+#### Partie sur le .log copié ####
+#### IMPORT dataset ####
+headerString = 'date time time-taken c-ip cs-username cs-auth-group x-exception-id sc-filter-result cs-categories cs(Referer) sc-status s-action cs-method rs(Content-Type) cs-uri-scheme cs-host cs-uri-port cs-uri-path cs-uri-query cs-uri-extension cs(User-Agent) s-ip sc-bytes cs-bytes x-virus-id'
+header = headerString.split(" ")
+a = pd.read_csv('./Data/bigTest.txt', delimiter="\s+", index_col=False, encoding="utf-8", skiprows=[0, 1, 2, 3, 4, 5], names=header)
+
+#### Adding statistics columns ####
+def add_url_size(df):
+  df["url_size"] = df["cs(Referer)"].apply(lambda x: len(str(x)))
+  return df
+
+def add_frequency(df, columnname):
+  columnnamefreq = str(columnname)+str("-frequency")
+  df_frequency = df.groupby([columnname]).count()
+  most_frequent = df_frequency.index.tolist()
+  quantity = df_frequency.iloc[:,0].tolist()
+  frequent_host_dict = dict(zip(most_frequent, quantity))
+
+  df[columnnamefreq] = df[columnname].apply(lambda x: frequent_host_dict.get(x))
+  #print(df.head())
+  return df
+
+def make_dummies(df, string):
+  df = pd.concat([df, pd.get_dummies(df[string], prefix=string)], axis=1)
+  df.drop([string], axis=1, inplace=True)
+  return df
+
+def compute_similarity(string1, stand_val):
+  indexes = np.where(string1 == stand_val)
+  return indexes[0]
+
+def apply_sim(data, column_name, one_line_df, new_col_name):
+  if column_name not in data.columns:
+    print("Wrong column name")
+    return 0
+  else:
+    list_indexes = compute_similarity(data[column_name].values, one_line_df[column_name].values[0])
+    data[new_col_name] = data.index.isin(list_indexes)
+    data[new_col_name] = data[new_col_name].astype(int)
+  return data
+
+def compute_all_sims(data, y):
+  all_cols = ['x-exception-id', 'cs-categories',
+              'sc-status', 's-action', 'cs-uri-scheme', 'cs-uri-port']
+  for col in all_cols:
+    apply_sim(data, col, y, 'sim')
+    if 'final_sim' not in data.columns:
+      data['final_sim'] = data['sim']
+    else:
+      data['final_sim'] = data['final_sim'] + data['sim']
+
+
+y=a.tail(1)
+a=make_dummies(a,'sc-filter-result')
+a = add_url_size(a)
+a = add_frequency(a,'cs-host')
+compute_all_sims(a,y)
+
+def normalization_zero_one(df, list_column):
+  for column in list_column:
+    df[column] = (df[column] - df[column].min()) / (df[column].max() - df[column].min())
+  return df
+
+def normalization_gauss(df, list_column):
+  for column in list_column:
+    df[column] = (df[column] - df[column].mean()) / df[column].std()
+  return df
+
+
+only_values = a[['cs-bytes','sc-bytes','url_size','cs-host-frequency','sc-filter-result_DENIED','sc-filter-result_OBSERVED','sc-filter-result_PROXIED']]
+'''
+only_values = only_values.dropna()
+only_values = only_values.loc[only_values['cs-bytes'] == '-', 'cs-bytes'] = ""
+only_values = only_values.loc[only_values['sc-bytes'] == '-', 'sc-bytes'] = ""
+'''
+only_values['cs-bytes'].apply(lambda x: isinstance(x, (int, float)))
+
+#only_values['cs-bytes'] = pd.to_numeric(only_values['cs-bytes'],errors='coerce').notnull()
+#only_values['sc-bytes'] = pd.to_numeric(only_values['sc-bytes'],errors='coerce').notnull()
+normalization_zero_one(only_values,['cs-bytes','sc-bytes','url_size','cs-host-frequency'])
+print(only_values.dtypes)
+####Getting the log that are sent periodically
+'''
 def time_interval(time1,time2):
   t1 = sum(x * int(t) for x, t in zip([3600, 60, 1], time1.split(":")))
   t2 = sum(x * int(t) for x, t in zip([3600, 60, 1], time2.split(":")))
   return abs(t1-t2)
 
-
-#Partie sur le .log copié
-headerString = 'date time time-taken c-ip cs-username cs-auth-group x-exception-id sc-filter-result cs-categories cs(Referer) sc-status s-action cs-method rs(Content-Type) cs-uri-scheme cs-host cs-uri-port cs-uri-path cs-uri-query cs-uri-extension cs(User-Agent) s-ip sc-bytes cs-bytes x-virus-id'
-header = headerString.split(" ")
-a = pd.read_csv('./Data/bigTest.txt', delimiter="\s+", index_col=False, encoding="utf-8", skiprows=[0, 1, 2, 3, 4, 5], names=header)
-a.insert(len(a.columns),"url_size", a["cs(Referer)"])
-a["url_size"] = a["url_size"].apply(lambda x: len(str(x)))
-a = a.sort_values(by=['url_size'],ascending=False)
-print(a.head(20))
-
-cs_host_frequency = a.groupby(['cs-host']).count().sort_values(by=["url_size"],ascending=False)
-most_frequent = cs_host_frequency.index.tolist()
-quantity = cs_host_frequency["date"].tolist()
-frequent_host_dict = dict(zip(most_frequent, quantity))
-
-a.insert(len(a.columns),"cs-host-frequency", a["cs-host"])
-a["cs-host-frequency"] = a["cs-host-frequency"].apply(lambda x: frequent_host_dict.get(x))
-print(a["cs-host-frequency"].head())
-#The max amount of log from the same user to get a better understanding
-#print(a["c-ip"].mode())
-b = a.sort_values(by=['c-ip', 'cs(Referer)'])
-#print(b.head(200))
-
-c = b[b["c-ip"]=="195e047d75339fd1"]
-#print(c.head(2000))
-'''
 lasttime = "14:45:02"
 lasturl = ""
 streak = 0
@@ -64,6 +123,16 @@ for index, row in b.iterrows():
 #the x-exception-id
 #print(a['x-exception-id'].unique())
 '''
+#### Some random stats to get to know the dataset ####
+
+#The max amount of log from the same user to get a better understanding
+#print(a["c-ip"].mode())
+#b = a.sort_values(by=['c-ip', 'cs(Referer)'])
+#print(b.head(200))
+
+#c = b[b["c-ip"]=="195e047d75339fd1"]
+#print(c.head(2000))
+
 
 '''
 ['-' 'internal_error' 'invalid_request' 'policy_denied' 'tcp_error'
