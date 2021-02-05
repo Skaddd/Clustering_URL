f999cce849a2d8668813db18d2c274f80cddc227
Timon <timon.fugier@orange.fr>
2021-02-05 09:17:42 +0100

    fusion des deux

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-
+#### Libraries ####
 import pandas as pd
-import  numpy as np
+import numpy as np
+import datetime
 from urllib.parse import urlparse
-import pandas as pd
+import httpagentparser
 
 pd.set_option('display.width', 40000)
 pd.set_option('display.max_columns', 40)
 pd.set_option('display.max_rows', 2000)
-#PARTIE UN PEU MOISIE
-'''
-a = pd.read_csv('message.txt',delimiter="\s+", index_col=False)
-a.head()
-cols = []
-a.drop(a.columns[cols], axis=1,inplace=True)
-print(a.iloc[2])
-a.head(25)
-'''
 
 #### Partie sur le .log copié ####
 #### IMPORT dataset ####
@@ -24,19 +16,39 @@ header = headerString.split(" ")
 data = pd.read_csv('./Data/bigTest.txt', delimiter="\s+", index_col=False, encoding="utf-8", comment= "#", names=header)
 data=data.drop(columns=['cs-username','cs-auth-group','date','time-taken','x-virus-id'])
 data = data.sample(frac=0.1)
-#### Adding statistics columns ####
-### FONCTIONS ###
+
+#### Preprocessing tools and removal of useless logs ####
 
 def preprocessing(df):
-  df.replace("-", "",inplace = True)
+  df.replace("-", "", inplace=True)
   df = df[pd.to_numeric(df['cs-bytes'], errors='coerce').notnull()]
   df = df[pd.to_numeric(df['sc-bytes'], errors='coerce').notnull()]
-  #df["cs(Referer)"] = df["cs(Referer)"].to_string()
   return df
 
-def add_url_size(df):
-  df["url_size"] = df["cs(Referer)"].apply(lambda x: len(str(x)))
-  return df
+
+def remove_rows(data):
+  data = data.loc[(data['x-exception-id'] == "")]
+  data.loc[data['cs-host'] == "", 'empty'] = 1
+  data['empty'] = data['empty'].fillna(0)
+  col_name_skip = ['referer-protocol', 'referer-domain', "referer-path", "cs-host"]
+
+  for col in data.columns:
+    if col in col_name_skip:
+      pass
+    elif col == 'cs-uri-path':
+      data.loc[data[col] == "/", "empty"] += 1
+
+    else:
+      data.loc[data[col] == "", "empty"] += 1
+  data = data.loc[data['empty'] < 6]
+  data = data.drop(columns=['empty', 'x-exception-id'])
+  return data
+
+data = preprocessing(data)
+data = remove_rows(data)
+
+#### Adding statistics columns ####
+### FONCTIONS ###
 
 #Fonction qui regarde si le status fait référence à une erreur
 def strange_status(a):
@@ -45,20 +57,13 @@ def strange_status(a):
   a['strange_status'] = np.where(a['sc-status'] < 399, 0, 1)
   return a
 
-#Fonction qui regarde si la méthode est comprise dans la liste des méthodes fréquentes
-def strange_method(a,tab_method):
-  a['strange_method'] = np.where(a['cs-method'].isin(tab_method), 0, 1)
-  return a
-
 #Fonction qui regarde si le port est compris dans la liste des ports fréquents
-def strange_port(a,tab_port):
-  a['strange_port'] = np.where(a['cs-uri-port'].isin(tab_port), 0, 1)
-  return a
+def strange_port(data, tab_port, tab_method):
+  data['strange_port'] = np.where(data['cs-uri-port'].isin(tab_port), 0, 1)
+  data.loc[(data['cs-method'].isin(tab_method)) & (data['strange_port'] == 1), 'strange_port'] += 1
 
-def find_same_referer(data):
-  data.loc[data['cs-host'] != data['referer-domain'], 'changed'] = 1
-  data['changed'] = data['changed'].fillna(0)
   return data
+
 
 def parse_url(data, column_toparse, scheme=True, domain=True, path=True, params=False, query=False, fragment=False):
   if column_toparse not in data.columns:
@@ -81,12 +86,18 @@ def parse_url(data, column_toparse, scheme=True, domain=True, path=True, params=
       pass
   return data
 
-def urlsize_superior_than(df, quantile1 = 0.95,quantile2 = 0.99,poids = 2):
+def find_same_referer(data):
+  data.loc[data['cs-host'] != data['referer-domain'], 'changed'] = 1
+  data['changed'] = data['changed'].fillna(0)
+  return data
 
-  df["url_size"] = df["cs(Referer)"].apply(lambda x: len(str(x)))
-  value1 = df["url_size"].quantile(quantile1)
-  value2 = df["url_size"].quantile(quantile2)
-  df["url_size"] = df["url_size"].apply(lambda x: max(0,min(poids,poids*((x-value1)/(value2-value1)))))
+def compute_url_size(df, poids):
+  sum_column = df["cs-host"].apply(lambda x: len(str(x))) + df["cs-uri-path"].apply(lambda x: len(str(x)))
+  df['url_size'] = sum_column
+  threshold1 = df["url_size"].quantile(0.9)
+  threshold2 = df["url_size"].quantile(0.99)
+  df["url_size"] = df["url_size"].apply(
+    lambda x: max(0, min(poids, poids * ((x - threshold1) / (threshold2 - threshold1)))))
   return df
 
 def is_big_cs_bytes(df, quantile1 = 0.95,quantile2 = 0.99,poids = 2):
@@ -95,14 +106,6 @@ def is_big_cs_bytes(df, quantile1 = 0.95,quantile2 = 0.99,poids = 2):
   value2 = df["bigcs"].quantile(quantile2)
   df["bigcs"] = df["bigcs"].apply(lambda x: max(0,min(poids,poids*((x-value1)/(value2-value1)))))
   return df
-
-def remove_savelogs(data):
-  if 'cs-uri-path' not in data.columns:
-    print("wrong database")
-    return 0
-  else:
-    data = data.loc[(data['cs-uri-path'] != "/") | (data['cs-host'] != "") | (data['sc-status'] != 0)]
-  return data
 
 def extension_superior_than(df, size, quantile, tab_extension):
   value = df["cs-uri-extension-frequency"].quantile(quantile)
@@ -118,7 +121,7 @@ def amount_people_by(df, columnname):
   columnname_by = str("people-by")+str(columnname)
   df[columnname_by] =  df.groupby([columnname]).nunique()
 
-def add_amount_people_by(df, columnname, quantile1=0.5, quantile2 = 0.99, poids = 3):
+def add_amount_people_by(df, columnname, quantile1=0.5, quantile2 = 0.90, poids = 3):
   columnname_by = str("people_by")+str(columnname)
   df2 = df.drop_duplicates(subset=[columnname,"c-ip"])
   df_frequency = df2.groupby([columnname]).count()
@@ -148,25 +151,42 @@ def add_frequency(df, columnname):
   #print(df.head())
   return df
 
+def compute_useragent(data, poids):
+  data['more_info'] = data['cs(User-Agent)'].apply(lambda x: httpagentparser.simple_detect(x))
+  data['fishy_os'] = data['more_info'].apply(lambda x: x.__contains__('Unknown OS'))
+  data['fishy_browser'] = data['more_info'].apply(lambda x: x.__contains__('Unknown Browser'))
+  data['fishy_os'] = (data['fishy_os'].astype(int)) * poids
+  data['fishy_browser'] = (data['fishy_browser'].astype(int)) * poids
+  return data
 
 #### APPLYING THE FUNCTIONS TO GET NEW COLOMNS ####
-data = preprocessing(data)
-data=parse_url(data,'cs(Referer)')
-data = find_same_referer(data)
-data = urlsize_superior_than(data)
-data = is_big_cs_bytes(data)
+
 data = strange_status(data)
-data = remove_savelogs(data)
+
 tab_method = ["GET","POST","HEAD","OPTIONS","PUT","CONNECT",""]
-data = strange_method(data,tab_method)
 tab_port = [80,443,"80","443",""]
-data = strange_port(data,tab_port)
+data = strange_port(data,tab_port,tab_method)
+
+data=parse_url(data,'cs(Referer)')
+
+data = find_same_referer(data)
+
+data=compute_url_size(data,2)
+
+data = is_big_cs_bytes(data)
+
 data = add_amount_people_by(data,"cs-host")
+
 data = add_frequency(data, "cs-uri-extension")
+
 tab_extension = ["dll","zip","rar","bin","exe"]
 data = extension_superior_than(data, 20,0.004,tab_extension)
-score_cols = ['url_size', 'bigcs', 'strange_status', 'strange_method', 'strange_port', 'people_bycs-host', 'changed','extension_strange']
+
+data=compute_useragent(data,1)
+
+score_cols = ['url_size', 'bigcs', 'strange_status', 'strange_port', 'people_bycs-host', 'changed','extension_strange', 'fishy_os', 'fishy_browser']
 data["sum"] = data[score_cols].sum(axis=1)
+
 data = data.sort_values(by=["sum"],ascending=False)
 
 print(data.head(200))
